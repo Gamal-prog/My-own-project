875f0bd087f42e7ae12b18d5505aea4936d14145
Gamal-prog <atemil791@gmail.com>
2023-11-06 20:47:02 +0600

    Forth Commit, made detail view

--- photo/views.py
+++ photo/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Photo
 
 def list_image(request):
     images = Photo.objects.all()
     return render(request, 'photo/photo_list.html', {'images': images})
 
-def image_detail(request):
-    image = Photo.objects.get()
+def image_detail(request, pk):
+    image = get_object_or_404(Photo, pk=pk)
     return render(request, 'photo/photo_detail.html', {'image': image})
