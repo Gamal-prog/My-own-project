677ca184db5a655c7a6267eb7df833d7b6bc14e0
Gamal-prog <atemil791@gmail.com>
2023-11-15 16:16:59 +0600

    Fifth Commit, a litle changes in home page and login, edit html files

--- photo/urls.py
+++ photo/urls.py
@@ -6,6 +6,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.list_image, name='image_list'),
     path('photo/<int:pk>/', views.image_detail, name='image_detail'),
+    path('photo/new/', views.image_new, name='image_new'),
+
+    #path('photo/<int:pk>/edit/', views.photo_edit, name='image_edit'),
+
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Photo
+from .forms import PhotoForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def list_image(request):
     images = Photo.objects.all()
@@ -8,3 +11,60 @@ def list_image(request):
 def image_detail(request, pk):
     image = get_object_or_404(Photo, pk=pk)
     return render(request, 'photo/photo_detail.html', {'image': image})
+
+def image_new(request):
+    form = PhotoForm()
+    return render(request, 'photo/photo_edit.html', {'form': form})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password,)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('image_list')
+        else:
+            messages.success(request, ("Try again, please"))
+            return redirect('login')
+    else:
+        return render(request, 'photo/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect('image_list')
+
+
+
+'''def photo_edit(request, pk):
+    image = get_object_or_404(Photo, pk=pk)
+    if request.method == "POST":
+        form = PhotoForm(request.POST, instance=image)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.author = request.user
+            image.save()
+            return redirect('photo_detail', pk=image.pk)
+        else:
+         form = PhotoForm(instance=image)
+    return render(request, 'blog/photo_edit.html', {'form': form})'''
+
+'''if request.method == "POST":
+        if 'save' in request.POST:
+            form = PhotoForm(request.POST)
+            form.save()'''
+
+
+'''def image_new(request):
+    if request.method == "POST":
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.author = request.user
+            image.save()
+            return redirect('photo_detail', pk=image.pk)
+    else:
+        form = PhotoForm()
+    return render(request, 'photo/photo_edit.html', {'form': form})'''
